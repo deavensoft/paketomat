89f69e07a91dee7139efe1c34744b22553996e51
Drazen Nikolic <drazen.nikolic@deavensoft.com>
2022-07-11 16:19:09 +0200

    Initial changes.

---
+++ src/main/java/com/deavensoft/paketomat/PaketomatApplication.java
@@ -0,0 +1,13 @@
+package com.deavensoft.paketomat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaketomatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaketomatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deavensoft/paketomat/PaketomatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deavensoft.paketomat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaketomatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
