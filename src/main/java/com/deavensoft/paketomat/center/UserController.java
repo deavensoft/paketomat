26ae483e234b0d6cb322b2e3362d0309b2162120
DusanGligorijevic <dukacode@gmail.com>
2022-07-19 10:26:23 +0200

    CRUD for User

--- src/main/java/com/deavensoft/paketomat/center/UserController.java
+++ src/main/java/com/deavensoft/paketomat/center/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/users")
@@ -20,8 +21,19 @@ public class UserController {
     }
     @PostMapping
     public void save(@RequestBody User u){
-       // User user = new User(u.getId(),u.getEmail(),u.getName(),u.getAddress());
         userService.save(u);
-
+    }
+    @GetMapping(path="{id}")
+    public Optional<User> findUserById(@PathVariable(name = "id") long id){
+       return userService.findUserById(id);
+    }
+    @DeleteMapping(path = "{id}")
+    public int deleteUser(@PathVariable(name = "id") Long id){
+        Optional<User> u = findUserById(id);
+        if (u.isEmpty())
+            System.out.println("there is no user with id:" + id);
+        userService.deleteUser(u.get());
+        System.out.println("User deleted");
+        return 1;
     }
 }
--- src/main/java/com/deavensoft/paketomat/center/iUser.java
+++ src/main/java/com/deavensoft/paketomat/center/iUser.java
@@ -9,5 +9,4 @@ import java.util.UUID;
 @Repository("user")
 public interface iUser extends JpaRepository<User, Long> {
 
-
 }
