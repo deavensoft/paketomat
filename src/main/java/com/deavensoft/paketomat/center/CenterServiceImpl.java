dc7840723db337e328283ecb8a630c997d6db24a
DobricMiroslava <miroslavaDobriic@gmail.com>
2022-07-27 09:23:37 +0200

    IBP-10_Package arrived into the distribution center:

--- src/main/java/com/deavensoft/paketomat/center/CenterServiceImpl.java
+++ src/main/java/com/deavensoft/paketomat/center/CenterServiceImpl.java
@@ -19,8 +19,6 @@ public class CenterServiceImpl implements CenterService {
     @Autowired
     public CenterServiceImpl(@Qualifier("center") CenterRepository centerRepository) {
         this.centerRepository = centerRepository;
-        this.distributiveCenter = new Center();
-        initialise();
     }
 
     public void initialise() {
