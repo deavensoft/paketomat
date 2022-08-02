0c33608763c4aacaeb506f757d2b8b9427d982fa
DusanGligorijevic <dukacode@gmail.com>
2022-08-02 14:15:35 +0200

    IBP-11_Finding_nearest_Paketomat_to_the_customer: bug fixing.

--- src/main/java/com/deavensoft/paketomat/email/EmailDetails.java
+++ src/main/java/com/deavensoft/paketomat/email/EmailDetails.java
@@ -3,14 +3,11 @@ package com.deavensoft.paketomat.email;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
-
 import java.util.Map;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Component
 public class EmailDetails {
     private String recipient;
     private String msgBody;
