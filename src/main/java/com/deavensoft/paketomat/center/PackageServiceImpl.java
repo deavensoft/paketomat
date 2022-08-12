d4977b143fff688a6afd2be46f1dc4566f17fb3e
AzureAD\StefanTomic <stefan.tomic@deavensoft.com>
2022-08-12 10:58:50 +0200

    Fixed the bugs

--- src/main/java/com/deavensoft/paketomat/center/PackageServiceImpl.java
+++ src/main/java/com/deavensoft/paketomat/center/PackageServiceImpl.java
@@ -24,8 +24,6 @@ public class PackageServiceImpl implements PackageService {
     private final PackageRepository packageRepository;
     private final EmailService emailService;
 
-    @Autowired
-
     public List<Package> getAllPackages() {
         return packageRepository.findAll();
     }
